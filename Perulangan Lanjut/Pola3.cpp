c505d4c55924cd50c5d0940612ceba5e6dcf8acf
PyAde <liodeo23@gmail.com>
2023-10-26 21:39:19 +0800

    Toki

---
+++ Perulangan Lanjut/Pola3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int a = 0;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<a%10;
+            a++;
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+}
