7863771dd9d3b7702fb945feac9b2fb08749a4ab
PyAde <liodeo23@gmail.com>
2023-11-22 20:11:00 +0800

    Toki

--- TLX/Addstr.cpp
+++ TLX/Addstr.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(){
 	string a,b;
 	cin>>a>>b;
+
 	cout<<a<<b;
+
 }
---
+++ TLX/Tzaph.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+	int B[N];
+	bool EQUAL = true;
+	bool NONDECREASING = true;
+	bool NONINCREASING = true;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin>>B[i];
+		if (B[i] != B[0])
+		{
+			
+		}
+		
+	}
+	
+}
